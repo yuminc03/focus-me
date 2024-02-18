74f40cedb1953c8e771c93c4224c2a1c7539579f
Yumin Chu <yuminc03@gmail.com>
2024-02-18 21:11:02 +0900

    Feat: Target Version 변경, 줄바꿈 간격 2로 수정

--- MySelf/MySelfApp.swift
+++ MySelf/MySelfApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct MySelfApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
