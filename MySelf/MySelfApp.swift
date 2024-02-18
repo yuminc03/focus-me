c2870348f0ea2255a930ba85068c6d4b0f23a3b3
Yumin Chu <yuminc03@gmail.com>
2024-02-18 21:04:52 +0900

    Initial Commit

---
+++ MySelf/MySelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySelfApp.swift
+//  MySelf
+//
+//  Created by Yumin Chu on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MySelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
