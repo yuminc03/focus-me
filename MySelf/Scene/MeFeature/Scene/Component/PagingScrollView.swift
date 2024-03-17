4d3deff2f7c8c70a3b65246324c3d05d5ecd54c7
Yumin Chu <yuminc03@gmail.com>
2024-03-17 23:05:45 +0900

    Feat: Card에 들어갈 내용 Entity, Paging ScrollView 추가

---
+++ MySelf/Scene/MeFeature/Scene/Component/PagingScrollView.swift
@@ -0,0 +1,51 @@
+//
+//  PagingScrollView.swift
+//  MySelf
+//
+//  Created by Yumin Chu on 3/17/24.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+
+struct PagingScrollCore: Reducer {
+  struct State: Equatable {
+    
+  }
+  
+  enum Action: Equatable {
+    
+  }
+  
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      return .none
+    }
+  }
+}
+
+struct PagingScrollView: View {
+  private let store: StoreOf<PagingScrollCore>
+  @ObservedObject private var viewStore: ViewStoreOf<PagingScrollCore>
+  
+  init(store: StoreOf<PagingScrollCore>) {
+    self.store = store
+    self.viewStore = ViewStore(store, observe: { $0 })
+  }
+  
+  var body: some View {
+    Text("")
+  }
+}
+
+
+#Preview {
+  PagingScrollView(store: .init(initialState: PagingScrollCore.State()) {
+    PagingScrollCore()
+  })
+}
+
+extension PagingScrollView {
+  
+}
--- MySelf/Scene/MeFeature/Scene/RootView.swift
+++ MySelf/Scene/MeFeature/Scene/RootView.swift
@@ -12,6 +12,24 @@ import ComposableArchitecture
 @Reducer
 struct RootCore {
   struct State: Equatable {
+    let cards: [Card] = [
+      .init(
+        title: "Introvert",
+        subTitle: "혼자 있고 싶은 나"
+      ),
+      .init(
+        title: "Intuition",
+        subTitle: "이상적이고 예측하는 능력이 있는 나"
+      ),
+      .init(
+        title: "Feeling",
+        subTitle: "상대방의 기분을 잘 알아차리고 위로를 잘 할 수 있는 나"
+      ),
+      .init(
+        title: "Judging",
+        subTitle: "일을 시작하기 전 미리 계획하고 순서를 정하는 나"
+      )
+    ]
     let id = UUID()
     let numberOfPages = 3
     @BindingState var currentPage = 0
