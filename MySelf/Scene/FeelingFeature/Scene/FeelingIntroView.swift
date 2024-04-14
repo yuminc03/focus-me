91ce278eb37da0a775e0286ea115d2e9292fbc63
Yumin Chu <yuminc03@gmail.com>
2024-04-14 22:45:10 +0900

    Feat: FeelingCoordinator, Screen 생성

---
+++ MySelf/Scene/FeelingFeature/Scene/FeelingIntroView.swift
@@ -0,0 +1,46 @@
+//
+//  FeelingIntroView.swift
+//  MySelf
+//
+//  Created by Yumin Chu on 4/14/24.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+
+struct FeelingIntroCore: Reducer {
+  struct State: Equatable {
+    let id = UUID()
+  }
+  
+  enum Action {
+    
+  }
+  
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      return .none
+    }
+  }
+}
+
+struct FeelingIntroView: View {
+  let store: StoreOf<FeelingIntroCore>
+  @ObservedObject var viewStore: ViewStoreOf<FeelingIntroCore>
+  
+  init(store: StoreOf<FeelingIntroCore>) {
+    self.store = store
+    self.viewStore = .init(store, observe: { $0 })
+  }
+  
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+#Preview {
+  FeelingIntroView(store: .init(initialState: FeelingIntroCore.State()) {
+    FeelingIntroCore()
+  })
+}
