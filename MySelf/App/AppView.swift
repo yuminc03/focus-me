bc4bb6394f3b2f9332072a071c65383c90840935
Yumin Chu <yuminc03@gmail.com>
2024-07-30 17:56:50 +0900

    Chore: 앱에서 필요없는 부분 모두 삭제

--- MySelf/App/AppView.swift
+++ MySelf/App/AppView.swift
@@ -1,38 +1,6 @@
-//
-//  AppView.swift
-//  MySelf
-//
-//  Created by Yumin Chu on 2/18/24.
-//
-
 import SwiftUI
 
-import ComposableArchitecture
-import TCACoordinators
-
-struct AppCore: Reducer {
-  struct State: Equatable {
-    
-  }
-  
-  enum Action {
-    
-  }
-  
-  var body: some ReducerOf<Self> {
-    EmptyReducer()
-  }
-}
-
 struct AppView: View {
-  private let store: StoreOf<AppCore>
-  @ObservedObject private var viewStore: ViewStoreOf<AppCore>
-  
-  init(store: StoreOf<AppCore>) {
-    self.store = store
-    self.viewStore = .init(store, observe: { $0 })
-  }
-  
   var body: some View {
     VStack(alignment: .leading, spacing: 20) {
       titleLabel
@@ -41,9 +9,7 @@ struct AppView: View {
 }
 
 #Preview {
-  AppView(store: .init(initialState: AppCore.State()) {
-    AppCore()
-  })
+  AppView()
 }
 
 extension AppView {
--- MySelf/MySelfApp.swift
+++ MySelf/MySelfApp.swift
@@ -1,19 +1,10 @@
-//
-//  MySelfApp.swift
-//  MySelf
-//
-//  Created by Yumin Chu on 2/18/24.
-//
-
 import SwiftUI
 
 @main
 struct MySelfApp: App {
   var body: some Scene {
     WindowGroup {
-      AppView(store: .init(initialState: AppCore.State()) {
-        AppCore()
-      })
+      AppView()
     }
   }
 }
