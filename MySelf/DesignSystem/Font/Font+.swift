4a4be1ad89920bdeae0cece4d0ee9721246347b7
Yumin Chu <yuminc03@gmail.com>
2024-07-31 13:59:57 +0900

    Refactor: 사용자 지정 폰트 사용방법 개선, Resource 폴더구조 수정

---
+++ MySelf/DesignSystem/Font/Font+.swift
@@ -0,0 +1,65 @@
+import SwiftUI
+
+import RswiftResources
+
+enum NotoSans {
+  case notoSansKRThin
+  case notoSansKRSemiBold
+  case notoSansKRRegular
+  case notoSansKRMedium
+  case notoSansKRLight
+  case notoSansKRExtraLight
+  case notoSansKRExtraBold
+  case notoSansKRBold
+  case notoSansKRBlack
+  
+  var resource: FontResource {
+    switch self {
+    case .notoSansKRThin:
+      return R.font.notoSansKRThin
+    case .notoSansKRSemiBold:
+      return R.font.notoSansKRSemiBold
+    case .notoSansKRRegular:
+      return R.font.notoSansKRRegular
+    case .notoSansKRMedium:
+      return R.font.notoSansKRMedium
+    case .notoSansKRLight:
+      return R.font.notoSansKRLight
+    case .notoSansKRExtraLight:
+      return R.font.notoSansKRExtraLight
+    case .notoSansKRExtraBold:
+      return R.font.notoSansKRExtraBold
+    case .notoSansKRBold:
+      return R.font.notoSansKRBold
+    case .notoSansKRBlack:
+      return R.font.notoSansKRBlack
+    }
+  }
+}
+
+extension Text {
+  /**
+   Custom Font 적용
+   ```
+   Text("Title")
+    .customFont(.notoSansKRRegular, size: 20)
+   ```
+   */
+  func customFont(
+    _ name: NotoSans = .notoSansKRRegular,
+    size: CGFloat = 16
+  ) -> Text {
+    self
+      .font(.custom(name.resource, size: size))
+  }
+}
+
+#Preview {
+  Group {
+    Text("Title")
+      .customFont(.notoSansKRBlack, size: 20)
+    
+    Text("제목")
+      .customFont(.notoSansKRMedium, size: 24)
+  }
+}
