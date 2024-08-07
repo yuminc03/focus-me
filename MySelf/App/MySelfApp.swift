baa6e8ce55d4901d3070dfb9a19e5829ee9c5957
Yumin Chu <yuminc03@gmail.com>
2024-08-07 11:30:19 +0900

    Feat: Firebase Project 생성, Project에서 사용설정

--- MySelf/App/MySelfApp.swift
+++ MySelf/App/MySelfApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct MySelfApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
   var body: some Scene {
     WindowGroup {
       NavigationStack {
