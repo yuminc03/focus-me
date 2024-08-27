548bc311f0686c40442b9c8e18eb26c16fb42f3f
Yumin Chu <yuminc03@gmail.com>
2024-08-27 22:56:01 +0900

    Feat: SelectionButton UI 수정

--- FocusMe/Scene/MainTab/HomeFeature/Scene/MBTITestFeature/Scene/MomentOfEnergy/Component/SelectionButton.swift
+++ FocusMe/Scene/MainTab/HomeFeature/Scene/MBTITestFeature/Scene/MomentOfEnergy/Component/SelectionButton.swift
@@ -6,6 +6,24 @@ extension SelectionButton {
     state.foregroundColor = color
     return state
   }
+  
+  func backgroundColor(_ color: Color) -> SelectionButton {
+    var state = self
+    state.backgroundColor = color
+    return state
+  }
+  
+  func pressedBgColor(_ color: Color) -> SelectionButton {
+    var state = self
+    state.pressedBgColor = color
+    return state
+  }
+  
+  func strokeColor(_ color: Color) -> SelectionButton {
+    var state = self
+    state.strokeColor = color
+    return state
+  }
 }
 /// 질문의 답변 버튼
 struct SelectionButton: View {
@@ -13,7 +31,9 @@ struct SelectionButton: View {
   private let action: () -> Void
   
   private var foregroundColor: Color = .deepPurple
-  
+  private var backgroundColor: Color = .white
+  private var pressedBgColor: Color = .skyBlueTheme
+  private var strokeColor: Color = .lavender
   
   init(title: String, action: @escaping () -> Void) {
     self.title = title
@@ -24,16 +44,33 @@ struct SelectionButton: View {
     Button(action: action) {
       Text(title)
         .customFont(.notoSansKRMedium, size: 14)
-        .foregroundColor(foregroundColor)
-        .padding(20)
-        .frame(maxWidth: .infinity)
-        .background(.white)
-        .overlay {
-          Capsule()
-            .strokeBorder(.lavender, lineWidth: 4)
-        }
-        .clipShape(Capsule())
-    }
+     }
+    .buttonStyle(SelectionButtonStyle(
+      foregroundColor: foregroundColor,
+      backgroundColor: backgroundColor,
+      pressedBgColor: pressedBgColor,
+      strokeColor: strokeColor
+    ))
+  }
+}
+
+struct SelectionButtonStyle: ButtonStyle {
+  let foregroundColor: Color
+  let backgroundColor: Color
+  let pressedBgColor: Color
+  let strokeColor: Color
+  
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .foregroundColor(foregroundColor)
+      .padding(20)
+      .frame(maxWidth: .infinity)
+      .background(configuration.isPressed ? pressedBgColor : backgroundColor)
+      .clipShape(Capsule())
+      .overlay {
+        Capsule()
+          .strokeBorder(strokeColor, lineWidth: 1)
+      }
   }
 }
 
