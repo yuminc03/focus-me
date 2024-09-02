5c2f7153513fac4f21a14215f3db3eee0e2c2247
Yumin Chu <yuminc03@gmail.com>
2024-09-02 17:43:25 +0900

    Feat: (중간저장) MBTI 테스트 완료 화면 UI

--- FocusMe/Scene/MainTab/HomeFeature/Scene/MBTITestFeature/Scene/CompleteTest/CompleteTestView.swift
+++ FocusMe/Scene/MainTab/HomeFeature/Scene/MBTITestFeature/Scene/CompleteTest/CompleteTestView.swift
@@ -30,9 +30,37 @@ struct CompleteTestView: View {
   
   var body: some View {
     WithPerceptionTracking {
-      Text("")
+      VStack(spacing: 20) {
+        TitleSection
+        
+        Spacer()
+        
+        
+      }
+      .padding(.horizontal, 20)
+      .backgroundColor()
+      .navigationBarHidden(true)
+      .onAppear {
+        NotiManager.post(key: .hideTab)
+      }
     }
   }
+}
+
+private extension CompleteTestView {
+  var TitleSection: some View {
+    VStack(spacing: 10) {
+      Text("MBTI 테스트가 완료되었습니다.")
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .customFont(.notoSansKRSemiBold, size: 24)
+      Text("이제 다음 화면에서 MBTI 결과를 확인해보세요!")
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .customFont(size: 12)
+    }
+    .foregroundColor(.deepPurple)
+  }
+  
+  
 }
 
 #Preview {
