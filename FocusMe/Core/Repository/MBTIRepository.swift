95c1ce707141b8e591cb6043772ffc05e39e38ab
Yumin Chu <yuminc03@gmail.com>
2024-09-09 23:53:01 +0900

    Feat: MBTI 테스트결과를 저장할 때 사용할 메서드 형태 추가

--- FocusMe/Core/Repository/MBTIRepository.swift
+++ FocusMe/Core/Repository/MBTIRepository.swift
@@ -3,5 +3,8 @@ import Foundation
 import FirebaseFirestore
 
 final class MBTIRepository {
-  
+  /// MBTI 테스트 결과 저장
+  func saveMBTIResult(target: CollectionTarget, collection: DocumentReference) async throws {
+    
+  }
 }
