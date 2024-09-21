93f6f52554b48f1d228347acbb35d6294385cd89
Yumin Chu <yuminc03@gmail.com>
2024-09-21 23:54:49 +0900

    Refactor: 필요없는 UserInfo 변수 삭제

--- FocusMe/Core/Entity/UserInfo.swift
+++ FocusMe/Core/Entity/UserInfo.swift
@@ -4,19 +4,10 @@ final class UserInfo {
   static let shared = UserInfo()
   
   var uid: String?
-  var email: String?
-  var name: String?
   
   private init() { }
   
-  func save(email: String?, name: String?) {
-    self.email = email
-    self.name = name
-  }
-  
   func clear() {
     uid = nil
-    email = nil
-    name = nil
   }
 }
